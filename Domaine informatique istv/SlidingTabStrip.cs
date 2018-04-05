e1e847853f4de81029fa6f020db84e9bcf7c5c3f
Yoxt <32191843+Yoxt@users.noreply.github.com>
2018-04-06 00:32:05 +0200

    Add files via upload

---
+++ Domaine informatique istv/SlidingTabStrip.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Domaine_informatique_istv
+{
+    public class SlidingTabStrip : LinearLayout
+    {
+        //Variables globales necessaire pour cette classe
+        private const int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 2;
+        private const byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0X26;
+        private const int SELECTED_INDICATOR_THICKNESS_DIPS = 8;
+        private int[] INDICATOR_COLORS = { 0x19A319, 0x0000FC };
+        private int[] DIVIDER_COLORS = { 0xC5C5C5 };
+
+        private const int DEFAULT_DIVIDER_THICKNESS_DIPS = 1;
+        private const float DEFAULT_DIVIDER_HEIGHT = 0.5f;
+
+        //Botton border
+        private int mBottomBorderThickness;
+        private Paint mBottomBorderPaint;
+        private int mDefaultBottomBorderColor;
+
+        //Indicator : little horizontal scrollbar
+        private int mSelectedIndicatorThickness;
+        private Paint mSelectedIndicatorPaint;
+
+        //Divider
+        private Paint mDividerPaint;
+        private float mDividerHeight;
+
+        //Selected position and offset
+        private int mSelectedPosition;
+        private float mSelectionOffSet;
+
+
+        //Tab colorizer
+        private SlidingTabScrollView.TabColorizer mCustomTabColorizer;
+        private SimpleTabColorizer mDefaultTabColorizer;
+
+
+        //Constructor
+        //Constructor
+        public SlidingTabStrip (Context context) : this(context, null)
+        { }
+        
+        public SlidingTabStrip (Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            SetWillNotDraw(false);
+
+            float density = Resources.DisplayMetrics.Density;
+
+            TypedValue outValue = new TypedValue();
+            context.Theme.ResolveAttribute(Android.Resource.Attribute.ColorForeground, outValue, true);
+            int ThemeForeGround = outValue.Data;
+            mDefaultBottomBorderColor = SetColorAlpha(ThemeForeGround, DEFAULT_BOTTOM_BORDER_COLOR_ALPHA);
+
+            mDefaultTabColorizer = new SimpleTabColorizer();
+            mDefaultTabColorizer.IndicatorColors = INDICATOR_COLORS;
+            mDefaultTabColorizer.DividerColors = DIVIDER_COLORS;
+
+            mBottomBorderThickness = (int)(DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS * density);
+            mBottomBorderPaint = new Paint();
+            mBottomBorderPaint.Color = GetColorFromInteger(0xC5C5C5); //Gray
+
+            mSelectedIndicatorThickness = (int)(SELECTED_INDICATOR_THICKNESS_DIPS * density);
+            mSelectedIndicatorPaint = new Paint();
+
+            mDividerHeight = DEFAULT_DIVIDER_HEIGHT;
+            mDividerPaint = new Paint();
+            mDividerPaint.StrokeWidth = (int)(DEFAULT_DIVIDER_THICKNESS_DIPS * density);
+
+        }
+
+        public SlidingTabScrollView.TabColorizer CustomTabColorizer
+        {
+            set
+            {
+                mCustomTabColorizer = value;
+                this.Invalidate();
+
+            }
+        }
+
+        public int [] SelectedIndicatorColors
+        {
+            set
+            {
+                mCustomTabColorizer = null;
+                mDefaultTabColorizer.IndicatorColors = value;
+                this.Invalidate(); 
+            }
+        }
+
+        public int [] DividerColors
+        {
+            set
+            {
+                mDefaultTabColorizer = null;
+                mDefaultTabColorizer.DividerColors = value;
+                this.Invalidate(); 
+            }
+        }
+        private Color GetColorFromInteger(int color)
+        {
+            return Color.Rgb(Color.GetRedComponent(color), Color.GetGreenComponent(color), Color.GetBlueComponent(color));
+        }
+
+        private int SetColorAlpha(int color, byte alpha)
+        {
+            return Color.Argb(alpha, Color.GetRedComponent(color), Color.GetGreenComponent(color), Color.GetBlueComponent(color));
+        }
+
+        public void OnViewPagerPageChanged(int position, float positionOffSet)
+        {
+            mSelectedPosition = position;
+            mSelectionOffSet = positionOffSet;
+            this.Invalidate();
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            int height = Height;
+            int tabCount = ChildCount;
+            int dividerHeightPx = (int)(Math.Min(Math.Max(0f, mDividerHeight), 1f) * height);
+            SlidingTabScrollView.TabColorizer tabColorizer = mCustomTabColorizer != null ? mCustomTabColorizer : mDefaultTabColorizer;
+
+            //Thick Colored underline below the current selection
+            if (tabCount > 0)
+            {
+                View SelectedTitle = GetChildAt(mSelectedPosition);
+                int left = SelectedTitle.Left;
+                int right = SelectedTitle.Right;
+                int color = tabColorizer.GetIndicatorColor(mSelectedPosition);
+
+                if (mSelectionOffSet > 0f && mSelectedPosition < (tabCount - 1))
+                {
+                    int nextColor = tabColorizer.GetIndicatorColor(mSelectedPosition + 1);
+                    if(color != nextColor)
+                    {
+                        color = blendColor(nextColor, color, mSelectionOffSet); 
+                    }
+
+                    View nextTitle = GetChildAt(mSelectedPosition + 1);
+                    left = (int)(mSelectionOffSet * nextTitle.Left + (1.0f - mSelectionOffSet) * left);
+                    right = (int)(mSelectionOffSet * nextTitle.Right + (1.0f - mSelectionOffSet) * right);
+                }
+
+                mSelectedIndicatorPaint.Color = GetColorFromInteger(color);
+
+                canvas.DrawRect(left, height - mSelectedIndicatorThickness, right, height, mSelectedIndicatorPaint);
+
+                //Create vertical dividers between tabs
+                int separatorTop = (height - dividerHeightPx) / 2;
+                for(int i = 0; i < ChildCount; i++)
+                {
+                    View child = GetChildAt(i);
+                    mDividerPaint.Color = GetColorFromInteger(tabColorizer.GetDividerColor(i));
+                    canvas.DrawLine(child.Right, separatorTop, child.Right, separatorTop + dividerHeightPx, mDividerPaint);
+                }
+
+                canvas.DrawRect(0, height - mBottomBorderThickness, Width, height, mBottomBorderPaint);
+            }
+        }
+
+        private int blendColor(int color1, int color2, float ratio)
+        {
+            float inverseRatio = 1f - ratio;
+            float r = (Color.GetRedComponent(color1) * ratio) + (Color.GetRedComponent(color2) * inverseRatio);
+            float g = (Color.GetGreenComponent(color1) * ratio) + (Color.GetGreenComponent(color2) * inverseRatio);
+            float b = (Color.GetBlueComponent(color1) * ratio) + (Color.GetBlueComponent(color2) * inverseRatio);
+
+            return Color.Rgb((int)r, (int)g, (int)b);
+        }
+
+        private class SimpleTabColorizer : SlidingTabScrollView.TabColorizer
+        {
+            private int[] mIndicatorColors;
+            private int[] mDividerColors;
+
+            public int GetIndicatorColor(int position)
+            {
+                return mIndicatorColors[position % mIndicatorColors.Length];
+            }
+
+            public int GetDividerColor(int position)
+            {
+                return mDividerColors[position % mDividerColors.Length];
+            }
+
+            public int[] IndicatorColors
+            { 
+                set { mIndicatorColors = value; }
+            }
+
+            public int[] DividerColors
+            {
+                set { mDividerColors = value; }
+            }
+        }
+    }
+}
---
+++ Domaine informatique istv/SlidingTabsFragment.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.View;
+using Java.Lang;
+using Android.Graphics;
+using Android.Text.Method;
+
+namespace Domaine_informatique_istv
+{
+    public class SlidingTabsFragment : Fragment
+    {
+        private SlidingTabScrollView mSlidingTabScrollView;
+        private ViewPager mViewPager;
+      
+        
+
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            return inflater.Inflate(Resource.Layout.fragment_sample, container, false);
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            mSlidingTabScrollView = view.FindViewById<SlidingTabScrollView>(Resource.Id.sliding_tabs);
+            mViewPager = view.FindViewById<ViewPager>(Resource.Id.viewpager);
+            mViewPager.Adapter = new SamplePagerAdapter();
+            mSlidingTabScrollView.ViewPager = mViewPager;
+
+        }
+
+        public class SamplePagerAdapter : PagerAdapter
+        {
+            List<string> items = new List<string>();
+            public int btn1 = 0;
+            private int btn2;
+            private int btn3;
+            private int btn4;
+
+            public SamplePagerAdapter() : base()
+            {
+                items.Add("Université");
+                items.Add("Contacts");
+                items.Add("Formations");
+                items.Add("Aide");
+                
+            }
+      
+
+            public override int Count
+            {
+                get { return items.Count; }
+            }
+
+            public override bool IsViewFromObject(View view, Java.Lang.Object obj)
+            {
+                return view == obj;
+            }
+ 
+            public override Java.Lang.Object InstantiateItem(ViewGroup container, int position)
+            {
+                       
+                View view1 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.pager_item, container, false);
+                View view2 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.pager_contact, container, false);
+                View view3 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.pager_formation, container, false);
+                View view4 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.pager_help, container, false);
+                View view5 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.licence_informatique, container, false);
+                View view6 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.deust_IOSI, container, false);
+                View view7 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.master_TNSI, container, false);
+                View view8 = LayoutInflater.From(container.Context).Inflate(Resource.Layout.master_IRCOMS, container, false);
+
+                if (position == 0)
+                {
+                    container.AddView(view1);
+                
+                    return view1;
+                }
+                else if (position == 1)
+                {       
+
+                    container.AddView(view2);                    
+                    return view2;
+                }
+                else if (position == 2)
+                {
+                    var i = 0;
+                    var button = view3.FindViewById<Button>(Resource.Id.formation_button1);
+                    var button1 = view3.FindViewById<Button>(Resource.Id.formation_button2);
+                    var button2 = view3.FindViewById<Button>(Resource.Id.formation_button3);
+                    var button3 = view3.FindViewById<Button>(Resource.Id.formation_button4);
+
+
+
+                    button.Click += (sender, args) => {
+                        
+                        this.btn1 = 1;
+
+                    };
+                    button1.Click += (sender, args) => {
+
+                        this.btn1 = 2;
+
+                    };
+                    button2.Click += (sender, args) => {
+
+                        this.btn1 = 3;
+
+                    };
+                    button3.Click += (sender, args) => {
+
+                        this.btn1 = 4;
+
+                    };
+
+
+                    if (this.btn1 == 1)
+                    {
+                        this.btn1 = 0;
+                        container.AddView(view5);
+                        return view5;
+
+                    }
+                    else if (this.btn1 == 2)
+                    {
+                        this.btn1 = 0;
+                        container.AddView(view6);
+                        return view6;
+
+                    }
+                    else if (this.btn1 == 3)
+                    {
+                        this.btn1 = 0;
+                        container.AddView(view7);
+                        return view7;
+
+                    }
+                   else  if (this.btn1 == 4)
+                    {
+                        this.btn1 = 0;
+                        container.AddView(view8);
+                        return view8;
+
+                    }
+                    else
+                    {
+                        container.AddView(view3);
+                        return view3;
+                    }
+                }
+                else
+                {
+                    container.AddView(view4);
+                     return view4;
+                }
+                    
+      
+            }
+
+            public string GetHeaderTitle (int position)
+            {
+                return items[position];
+            }
+            public override void DestroyItem(ViewGroup container, int position, Java.Lang.Object obj)
+            {
+                container.RemoveView((View)obj); //On sait que obj sera un view
+            }
+        }
+    }
+ }
